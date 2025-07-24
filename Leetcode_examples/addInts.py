b0bbe64b07b531247f5011513be045500e4d36bb
eyllsahin <eylul.sahin@std.ankaramedipol.edu.tr>
2025-07-24 15:06:19 +0300

    added some code snippets

---
+++ Leetcode_examples/addInts.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        return num1 + num2
+
+
+num1 = 12
+num2 = 5
+answer = Solution().sum(num1, num2)
+print(answer)
+
--- Python OOP/Classes_and_Instances.py
+++ Python OOP/Classes_and_Instances.py
@@ -24,19 +24,22 @@ print(emp_2.email)
 
 
 class Employee:
-    def __init__(self,first,last,email,pay): #constructor
-        self.first=first
-        self.last=last
-        self.email=email
-        self.pay=pay
+    def __init__(self, first, last, email, pay):  #constructor
+        self.first = first
+        self.last = last
+        self.email = email
+        self.pay = pay
+
     def fullname(self):
-        return f'{self.first} {self.last}' #or return '{} {}'.format(self.first, self.last)
-emp_1=Employee('Corey','Smith','123@gmail.com',50000)
-emp_2=Employee('Colly','Smog','45@gmail.com',50000)
+        return f'{self.first} {self.last}'  #or return '{} {}'.format(self.first, self.last)
+    def apply_raise(self):
+        self.pay=int(self.pay * 1.15)
 
+emp_1 = Employee('Corey', 'Smith', '123@gmail.com', 50000)
+emp_2 = Employee('Colly', 'Smog', '45@gmail.com', 50000)
 
-print(emp_1.email)
-print(emp_2.email)
+#print(emp_1.email)
+#print(emp_2.email)
 #print('The name is:{} {} Email: {} Pay: {}'.format(emp_1.first,emp_1.last, emp_1.email, emp_1.pay))
 #since the code above is too long to type the info, we create a method that does that
-print(emp_1.fullname()) #usage of the method
+#print(emp_1.fullname()) #usage of the method
